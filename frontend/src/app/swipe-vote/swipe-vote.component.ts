552c19973a08da0cd5875a64878ae34070ae7817
jonas.goetz01@web.de <jonas.goetz01@web.de>
2024-09-28 19:28:53 +0000

    splid the screen

---
+++ frontend/src/app/swipe-vote/swipe-vote.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-swipe-vote',
+  standalone: true,
+  imports: [],
+  templateUrl: './swipe-vote.component.html',
+  styleUrl: './swipe-vote.component.scss'
+})
+export class SwipeVoteComponent {
+
+}
